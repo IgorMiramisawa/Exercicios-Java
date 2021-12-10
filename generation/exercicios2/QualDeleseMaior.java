2015484601d83a1642f35bd1ecd11d8cf9471ae2
Igor <Igor.miramisawa@outlook.com>
2021-12-09 23:08:30 -0300

    Exercicios 2

--- generation/exercicios2/QualDeleseMaior.java
+++ generation/exercicios2/QualDeleseMaior.java
@@ -6,23 +6,36 @@ public class QualDeleseMaior {
 	public static void main (String [] args) {
 		Scanner leia = new Scanner(System.in);
 		
-		int contador , valor1 ,  maiorvalor = 0;
-		contador = 0;
-		while (contador < 3)  {
-			System.out.println("Digite um valor");
-			valor1 = leia.nextInt();
-			contador++;
-			if (valor1 > maiorvalor){
-				maiorvalor = valor1;
+		double valor1, valor2 ,valor3 ;
+		
+		System.out.println("Digite o primeiro valor");
+		valor1 = leia.nextInt();
+		System.out.println("Digite o segundo valor");
+		valor2 = leia.nextInt();
+		System.out.println("Digite o terceiro valor");
+		valor3 = leia.nextInt();
+		
+		leia.close();
+		
+		if (valor1 >= valor2 && valor1 >= valor3){
+			System.out.println("O maior valor inserido foi o " + valor1);
+		}else if (valor2 >= valor1 && valor2 >= valor3) {
+			System.out.println("O maior valor inserido foi o " + valor2);
+		}else 
+			System.out.println("O maior valor inserido foi o " + valor3);
+		
+		
+		
+			
+		
+				
 			
 			}
 		
-		}
-		leia.close();	
-		System.out.println("O maior valor inserido foi " + maiorvalor);
+		
 		
 		
 		
 		
 	}
-}
+
