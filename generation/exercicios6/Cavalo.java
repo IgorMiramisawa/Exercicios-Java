9f43fa213e04b22872be25b8a6a3da46dc70b395
Igor <Igor.miramisawa@outlook.com>
2021-12-15 13:29:11 -0300

    Exercicios 6

---
+++ generation/exercicios6/Cavalo.java
@@ -0,0 +1,13 @@
+package br.com.generation.exercicios6;
+
+public class Cavalo  extends Animal{
+	
+	public void somcavalo () {
+		System.out.println("O cavalo está relichando");
+	}
+	public void cavalocorrendo () {
+		System.out.println("Cavalo :Pocótó Pocótó Pocótó");
+	}
+	
+	
+}
